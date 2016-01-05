86cb43af785e6b92bd80052ff03817e9fd3a1f3c
Rudzainy Rahman <rudzainy@gmail.com>
2016-01-06 02:59:08 +0800

    Add seed data for User and Listing tables excluding images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# Create admin
+User.create(email: "tom@cruise.com", password: "123456")
+
+# Creates dummy users
+5.times do
+	User.create(email: Faker::Internet.email, password: "123456")
+end
+
+# Creates random number of listings
+User.all.each do |user|
+	rand(1..3).times do
+		user.listings.create(name: Faker::Hipster.sentence(3),
+												 bedroom: rand(1..5),
+												 accomodates: rand(2..5),
+												 location: Faker::Address.country,
+												 price_per_night: rand(80..250))
+	end
+end
+
