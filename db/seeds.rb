078710130d5410f11d76cc7d504d4247a5b91d29
Rudzainy Rahman <rudzainy@gmail.com>
2016-01-10 19:18:38 +0800

    Import assets, update index page layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -16,13 +17,16 @@ User.create(email: "tom@cruise.com", password: "123456")
 end
 
 # Creates random number of listings
+
 User.all.each do |user|
 	rand(1..3).times do
-		user.listings.create(name: Faker::Hipster.sentence(3),
+		listing = user.listings.create(name: Faker::Hipster.sentence(3),
 												 bedroom: rand(1..5),
 												 accomodates: rand(2..5),
 												 location: Faker::Address.country,
 												 price_per_night: rand(80..250))
+		listing.images = [Pathname.new(Rails.root + 'app/assets/images/sample_listing_image.jpg').open]
+		listing.save!
 	end
 end
 
